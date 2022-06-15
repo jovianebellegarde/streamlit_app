7456daae70ba6890ace4656d6b237ad059c5d464
jbellegarde <bellegarde.j@northeastern.edu>
2022-06-15 12:06:14 -0400

    hoping i didn't break anything

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,9 @@
-import streamlit
 import streamlit as st
 import pandas as pd
 import requests
 import snowflake.connector
 from urllib.error import URLError
-
-st.title('My Parents New Healthy Diner')
-st.header('Breakfast Favorites')
-st.text('🥣 Omega 3 & Blueberry Oatmeal')
-st.text('🥗 Kale, Spinach & Rocket Smoothie')
-st.text('🐔 Hard-Boiled Free-Range Egg')
-st.text('🥑🍞 Avocado Toast')
-
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com"
-                         "/dabw/fruit_macros.txt")
-fruit_list = fruit_list.set_index('Fruit')
-
-# put a pick list so user can pick the fruit they want to include
-fruits_selected = st.multiselect('Pick some fruits:', list(fruit_list.index),
-                                 ['Avocado', 'Strawberries'])
-fruits_to_show = fruit_list.loc[fruits_selected]
-
-# display table
-st.dataframe(fruits_to_show)
+MY_CNX = snowflake.connector.connect(**st.secrets["snowflake"])
 
 
 def get_fruityvice_data(this_fruit_choice):
@@ -34,45 +14,70 @@ def get_fruityvice_data(this_fruit_choice):
     return fruityvice_normalized
 
 
-# new section to display fruityvice api response
-st.header('Fruityvice Fruit Advice!')
-
-try:
-    fruit_choice = st.text_input('What fruit would you like '
-                                 'information about?')
-    if not fruit_choice:
-        st.error('Please select a fruit to get information')
-    else:
-        fruity_vice_data = get_fruityvice_data(fruit_choice)
-        st.dataframe(fruity_vice_data)
-
-except URLError as e:
-    st.error()
-
-
 # Snowflake-related functions
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
+    with MY_CNX.cursor() as my_cur:
         my_cur.execute('select * from fruit_load_list')
         return my_cur.fetchall()
 
 
-# add a button to load the fruit
-if st.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    st.dataframe(my_data_rows)
-
-
 # allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
+    with MY_CNX.cursor() as my_cur:
         my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
         return f'Thanks for adding {new_fruit}'
 
 
-add_my_fruit = st.text_input('What fruit would you like to add?')
-if st.button('Add a Fruit to the List'):
-    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-    added_fruit = insert_row_snowflake(add_my_fruit)
-    st.text(added_fruit)
+def main():
+
+    st.title('My Parents New Healthy Diner')
+    st.header('Breakfast Favorites')
+    st.text('🥣 Omega 3 & Blueberry Oatmeal')
+    st.text('🥗 Kale, Spinach & Rocket Smoothie')
+    st.text('🐔 Hard-Boiled Free-Range Egg')
+    st.text('🥑🍞 Avocado Toast')
+
+    st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+    fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com"
+                             "/dabw/fruit_macros.txt")
+    fruit_list = fruit_list.set_index('Fruit')
+
+    # put a pick list so user can pick the fruit they want to include
+    fruits_selected = st.multiselect('Pick some fruits:', list(fruit_list.index),
+                                     ['Avocado', 'Strawberries'])
+    fruits_to_show = fruit_list.loc[fruits_selected]
+
+    # display table
+    st.dataframe(fruits_to_show)
+
+
+    # new section to display fruityvice api response
+    st.header('Fruityvice Fruit Advice!')
+
+    try:
+        fruit_choice = st.text_input('What fruit would you like '
+                                     'information about?')
+        if not fruit_choice:
+            st.error('Please select a fruit to get information')
+        else:
+            fruity_vice_data = get_fruityvice_data(fruit_choice)
+            st.dataframe(fruity_vice_data)
+
+    except URLError as e:
+        st.error()
+
+    # add a button to load the fruit
+    if st.button('Get Fruit Load List'):
+        my_cnx = MY_CNX
+        my_data_rows = get_fruit_load_list()
+        my_cnx.close()
+        st.dataframe(my_data_rows)
+
+    add_my_fruit = st.text_input('What fruit would you like to add?')
+    if st.button('Add a Fruit to the List'):
+        # my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+        added_fruit = insert_row_snowflake(add_my_fruit)
+        st.text(added_fruit)
+
+
+main()
