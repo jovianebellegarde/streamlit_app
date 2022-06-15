e3c420803e258b5fb15c7dc083ba91ed95b9f068
jbellegarde <bellegarde.j@northeastern.edu>
2022-06-15 13:15:26 -0400

    created function for getting fruity vice

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,15 @@ fruits_to_show = fruit_list.loc[fruits_selected]
 # display table
 st.dataframe(fruits_to_show)
 
+
+# creating a function
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get(
+        "https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+
 # new section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
 try:
@@ -31,10 +40,8 @@ try:
     if not fruit_choice:
         st.error('Please select a fruit to get information.')
     else:
-        fruityvice_response = requests.get(
-            "https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-        st.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        st.dataframe(back_from_function)
 except URLError as e:
     st.error()
 
@@ -44,8 +51,7 @@ st.write(f'The user entered {fruit_choice}')
 # don't run anything past here while we troubleshoot
 st.stop()
 
-
-#connector
+# connector
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
