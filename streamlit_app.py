1a4df3f08026450ad086967785e0a3949a037bda
jbellegarde <bellegarde.j@northeastern.edu>
2022-06-15 11:25:21 -0400

    moved second choice into functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,9 +49,6 @@ try:
 except URLError as e:
     st.error()
 
-# don't run anything past here while debugging
-#st.stop()
-
 
 # Snowflake-related functions
 def get_fruit_load_list():
@@ -66,3 +63,16 @@ if st.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     st.dataframe(my_data_rows)
 
+
+# allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return f'Thanks for adding {new_fruit}'
+
+
+add_my_fruit = st.text_input('What fruit would you like to add?')
+if st.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+    added_fruit = insert_row_snowflake(add_my_fruit)
+    st.text(added_fruit)
