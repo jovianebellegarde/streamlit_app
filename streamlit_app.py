4a75a2c60641b2a48d18eb2c6e7e87474e5eaf4b
jbellegarde <bellegarde.j@northeastern.edu>
2022-06-15 13:10:58 -0400

    put in try/except block

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,20 @@ st.dataframe(fruits_to_show)
 
 # new section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
-fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
+try:
+    fruit_choice = st.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        st.error('Please select a fruit to get information.')
+    else:
+        fruityvice_response = requests.get(
+            "https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        st.dataframe(fruityvice_normalized)
+except URLError as e:
+    st.error()
+
+
 st.write(f'The user entered {fruit_choice}')
-
-fruityvice_response = requests.get(
-    "https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# take the json version of the response and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-
-# output the screen as a table
-st.dataframe(fruityvice_normalized)
-
-
 
 # don't run anything past here while we troubleshoot
 st.stop()
